17e759a3922f8fd3bd5665eb7be2e9941c7386b2
tgprudencio <thiago.gprudencio@gmail.com>
2022-08-18 22:55:50 -0300

    feat: finished project

--- constants/theme.js
+++ constants/theme.js
@@ -5,11 +5,11 @@ export const COLORS = {
     primary: "#7F5DF0",     // Light purple
     secondary: "#5D2DFD",   // Dark purple
 
-    white: "#fff",
-    black: "#000000",
+    white: "#F3F2ED",
+    black: "#38424B",
     green: "#37E39F",
     red: "#F9A8BA",
-    gray: "#6A6A6A",
+    gray: "#8E9397",
     lightGray: "#dbdbdb",
     lightGray1: "#f5f6fa"
 };
